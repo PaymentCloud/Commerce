9602430bb2ba4db88d62e81c546a07e787e9054b
Willy Alexander Gonzalez Contreras <alexgonzalez_contreras@outlook.com>
2018-08-01 20:02:14 -0600

    Features/ First commit

---
+++ app/src/main/java/paymentcloud/creaj/sv/com/comercio/Login.java
@@ -0,0 +1,192 @@
+package paymentcloud.creaj.sv.com.comercio;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by alexg on 11/05/2018.
+ */
+
+public class Login extends AppCompatActivity {
+
+    String em,pw;
+    private ProgressDialog pDialog;
+    final protected static char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //TextView tv2 = this.findViewById(R.id.tv2);
+
+        Button btnLogin = this.findViewById(R.id.btn_login);
+
+        final EditText username = this.findViewById(R.id.input_email);
+        final EditText password = this.findViewById(R.id.input_balance);
+        final Button readNFCButton = this.findViewById(R.id.readNFC);
+
+
+        pDialog = new ProgressDialog(this);
+
+        pDialog.setMessage("Espere un momento");
+        pDialog.setCancelable(false);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                em=username.getText().toString();
+                pw= password.getText().toString();
+                if(em.length()>4&&pw.length()>2){
+                    LoginServer();
+                }
+                else{
+                    Snackbar.make(v, "Ingrese los datos por favor", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+
+            }
+        });
+
+        boolean isLogged = false;
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        isLogged = preferences.getBoolean("is_logged",false);
+        if(isLogged){
+            Intent i = new Intent(this, Main2Activity.class);
+            startActivity(i);
+            finish();
+        }
+
+
+
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        int v;
+        for ( int j = 0; j < bytes.length; j++ ) {
+            v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+
+    public void LoginServer(){
+        showpDialog();
+        try {
+            final String _response = "";
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = "http://koalafood.com/api/paymentgateway/v2/public/LoginBusiness";
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("----respuesta",response.toString());
+                            try{
+
+                                JSONArray ja =new JSONArray(response);
+                                JSONObject profile = new JSONObject(ja.get(0).toString());
+                                String id = profile.getString("user_id");
+
+                                String name = profile.getString("name");
+                                String mail = profile.getString("mail");
+                                String phone = profile.getString("id_wallet");
+
+                                Log.d("uid",id.toString());
+                                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                                final SharedPreferences.Editor editor = preferences.edit();
+                                editor.putString("user_id", String.valueOf(id));
+
+                                editor.putString("user_name", String.valueOf(name));
+                                editor.putString("user_mail", String.valueOf(mail));
+                                editor.putString("id_wallet", String.valueOf(phone));
+
+                                editor.putBoolean("is_logged", true);
+                                editor.commit();
+                                hidepDialog();
+                                Intent i = new Intent(Login.this, Main2Activity.class);
+                                startActivity(i);
+                            } catch (Exception s){
+                                Log.d("ERROR",s.toString());
+                                if(response.equals("{'login': 'false'}")){
+                                    hidepDialog();
+                                    Toast.makeText(getApplicationContext(), "Error en los datos ingresados, intenta nuevamente", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // _response.setText("That didn't work!");
+                    Log.d("respuesta error",error.toString());
+                }
+            }) {
+                //adding parameters to the request
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+
+
+                    params.put("email", em.toString());
+                    params.put("pwd", pw.toString());
+
+
+                    Log.d("PARAMETROS",params.toString());
+                    return params;
+                }
+            };
+            queue.add(stringRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),
+                    "Error: " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+
+
+}
---
+++ app/src/main/java/paymentcloud/creaj/sv/com/comercio/Main2Activity.java
@@ -0,0 +1,252 @@
+package paymentcloud.creaj.sv.com.comercio;
+
+import android.app.PendingIntent;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+    String em,pw;
+    TextView txtbussinessid;
+    private ProgressDialog pDialog;
+    final protected static char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    private NfcAdapter mNfcAdapter;
+    int accion =0;
+    String name, id;
+    EditText username, credittocharge;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        txtbussinessid = this.findViewById(R.id.bussinessid);
+        Button btnCharge = this.findViewById(R.id.btn_charge);
+
+        username = this.findViewById(R.id.input_NFC);
+        credittocharge = this.findViewById(R.id.input_balance);
+        final Button readNFCButton = this.findViewById(R.id.readNFC);
+
+        //Preferences
+        boolean isLogged = false;
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        isLogged = preferences.getBoolean("is_logged",false);
+        name = preferences.getString("name", null);
+        id = preferences.getString("user_id", null);
+
+
+        readNFCButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                accion=4;
+
+            }
+        });
+
+        btnCharge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (username.length()>3 && credittocharge.length()>0){
+
+                        Chargewallet(credittocharge.getText().toString(),username.getText().toString(),id);
+                    }else{
+                        Toast.makeText(Main2Activity.this, "Datos no validos ", Toast.LENGTH_SHORT).show();
+                    }
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Espere un momento");
+        pDialog.setCancelable(false);
+
+        initNFC();
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        IntentFilter techDetected = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        IntentFilter[] nfcIntentFilter = new IntentFilter[]{techDetected,tagDetected,ndefDetected};
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        if(mNfcAdapter!= null)
+            mNfcAdapter.enableForegroundDispatch(this, pendingIntent, nfcIntentFilter, null);
+
+    }
+    private void initNFC(){
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        if(tag != null) {
+            if(accion==0){
+                Toast.makeText(this, "Etiqueta detectada, seleccione una acción", Toast.LENGTH_SHORT).show();
+            }
+            else{
+
+                Ndef ndef = Ndef.get(tag);
+                Tag myTag = (Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                Log.i("tag ID ", myTag.getId().toString());
+                String vtag= bytesToHex(myTag.getId());
+                Log.i("tax ", vtag);
+
+                if(accion==1){
+
+                }
+                if(accion==2){
+
+                }
+                if(accion==3){
+
+                }
+                if(accion==4){
+                    username.setText(vtag);
+                }
+
+                if (ndef == null) {
+                    return;
+                }
+
+            }
+
+
+
+
+        }
+    }
+
+    public void Chargewallet(final String credit, final String nfc, final String businessid){
+        showpDialog();
+        try {
+            final String _response = "";
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = "http://koalafood.com/api/paymentgateway/v2/public/chargeWallet";
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("----respuesta",response.toString());
+                            hidepDialog();
+
+                            try{
+
+
+                                JSONObject profile = new JSONObject(response);
+
+                                String amount = profile.getString("amount");
+                                String pnfc_tag = profile.getString("pnfc_tag");
+                                Toast.makeText(Main2Activity.this, pnfc_tag, Toast.LENGTH_SHORT).show();
+                                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                                final SharedPreferences.Editor editor = preferences.edit();
+                                editor.putString("charged", String.valueOf(amount));
+                                editor.putString("pnfc_tag", String.valueOf(pnfc_tag));
+                                editor.commit();
+                                Intent i = new Intent(Main2Activity.this, Summary.class);
+                                startActivity(i);
+
+                                hidepDialog();
+
+
+                            } catch (Exception s){
+                                Log.d("ERROR",s.toString());
+                                if(response.equals("{'login': 'false'}")){
+                                    hidepDialog();
+                                    Toast.makeText(getApplicationContext(), "Error en los datos ingresados, intenta nuevamente", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // _response.setText("That didn't work!");
+                    Log.d("respuesta error",error.toString());
+                }
+            }) {
+                //adding parameters to the request
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("amount", credit);
+                    params.put("user_id", businessid);
+                    params.put("pnfc_tag", nfc);
+
+                    return params;
+                }
+            };
+            queue.add(stringRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),
+                    "Error: " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        int v;
+        for ( int j = 0; j < bytes.length; j++ ) {
+            v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+
+
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+
+
+}
